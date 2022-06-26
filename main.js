017bda56714066226f4c5f493034eb9cf4919cf2
victor <jvictorpc@gmail.com>
2022-06-26 15:46:06 -0300

    alguns comentários sobre o que foi visto nessa aula

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+/**
+ * O objeto document serve como um ponto de entrada para acessar o conteúdo da página(a árvore DOM)
+ *  - E também oferece algumas funções globais ao documento
+ * 
+ * função QuerySelector
+ *  - retorna o primeiro elemento do documento que corresponde ao que foi especificado 
+ * 
+ */
 let teclaPom = document.querySelector('.tecla_pom');
 console.log(teclaPom);
