f53def20375f92cfbf5ff557d2b1f8dd7c38d4ff
victor <jvictorpc@gmail.com>
2022-06-27 11:41:18 -0300

    aula sobre funcoes em javascript

--- main.js
+++ main.js
@@ -6,5 +6,12 @@
  *  - retorna o primeiro elemento do documento que corresponde ao que foi especificado 
  * 
  */
+
+function tocaSomPom (){
+     document.querySelector('#som_tecla_pom').play();
+}
 let teclaPom = document.querySelector('.tecla_pom');
-console.log(teclaPom);
+
+teclaPom.addEventListener('click', tocaSomPom);
+
+
